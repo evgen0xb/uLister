adc4234379b259aa9de63353d826de4cbb0010c4
Evgeny <evgeny@dot.com>
2026-07-12 00:33:18 +0300

    added the ESC key to exit full-screen mode

--- doc/readme_en.txt
+++ doc/readme_en.txt
@@ -272,6 +272,7 @@ Ctrl+I:
 
 In version 4.4.0.0, you can switch to and from full-screen mode (even from Quick View) in any document format.
 Use the F11 or Alt+Enter hotkeys, or use the mouse right-click context menu item.
+The ESC key is used to exit full-screen mode (in normal mode, it closes the viewing window as usual).
 
 8. Compilation.
 Use the vs2015.sln file to edit the project's source code in modern versions of Visual Studio.
@@ -592,3 +593,6 @@ Finally, the search procedure has been rewritten:
     - full-screen mode support has been implemented for all document types (via a context menu item)
     - refactoring of the Full Screen mode
     - added hotkeys Alt+Enter and F11 to switch to full-screen mode
+
+2026-07-12
+    - added the ESC key to exit full-screen mode (in normal mode it closes the viewing window as usual)
--- src/window.cpp
+++ src/window.cpp
@@ -246,6 +246,13 @@ LRESULT CALLBACK SccviewerWindowProc(HWND hWnd, UINT message, WPARAM wParam, LPA
 				mydata->FullScreen.ChangeFullScrMode();
 				return 0;
 			}
+			if (lParam == VK_ESCAPE)
+				if (mydata->FullScreen.isFullScrEnabled()) // only in full screen mode else post ESC end other KEY messages
+				{
+					//OutputDebugStringA("Exit Full Screen");
+					mydata->FullScreen.ExitFromFullScrMode();
+					return 0;
+				}
 
 			PostMessage(mydata->TListerWindow, WM_KEYDOWN, lParam, 0); // TC Lister Handler: (Ctrl+F/F7, F3/Shift+F3, ESC)
 			break;
@@ -401,24 +408,19 @@ LRESULT CALLBACK SccdisplayWindowProc(HWND hWnd, UINT message, WPARAM wParam, LP
 			if (wParam & MK_CONTROL) { mydata->ZoomBitmapVecFont(UlisterZoom::ZRESET); return 0; }
 			break;
 		case WM_SYSKEYDOWN:
-		{
 			if ((wParam == VK_RETURN) && (GetKeyState(VK_MENU) < 0))
 			{
 				//OutputDebugStringA("Alt+Enter - Change Full Screen [English keyboard layout]");
 				mydata->FullScreen.ChangeFullScrMode();
 				return 0;
 			}
-		}
 		case WM_KEYDOWN: // 'WM_KEYDOWN' message <-- fix Right-Alt + Enter in international layout
-		{
-			bool isAltPressed = (GetKeyState(VK_MENU) < 0);
-			if ((wParam == VK_RETURN) && isAltPressed)
+			if ((wParam == VK_RETURN) && (GetKeyState(VK_MENU) < 0))
 			{
 				//OutputDebugStringA("Alt+Enter - Change Full Screen [Russian keyboard layout]");
 				mydata->FullScreen.ChangeFullScrMode();
 				return 0;
 			}
-		}
 		} // switch
 		return CallWindowProc(mydata->OriginalSccdisplayWindowProc, hWnd, message, wParam, lParam);
 	}
